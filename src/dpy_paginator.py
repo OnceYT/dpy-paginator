0c9396a662a961a014a49a0a21c4d3839a8135af
OnceYT <80458521+OnceYT@users.noreply.github.com>
2023-03-02 00:09:11 +0530

    Renamed objects and added type hints to paginate()

--- src/dpy_paginator.py
+++ src/dpy_paginator.py
@@ -4,12 +4,12 @@
 import discord
 from discord.ext import commands
 
-class ModalPage(discord.ui.Modal, title = "Jump to page"):
+class modal(discord.ui.Modal, title = "Jump to page"):
   def __init__(self, embeds: list, author_ids: list, timeout: int):
     super().__init__(timeout = timeout)
     self.embeds = embeds
     self.author_ids = author_ids
-    self.input.label = f"Page number: (0-{len(self.embeds)})"
+    self.input.label = f"Page number: (1-{len(self.embeds)})"
 
   input = discord.ui.TextInput(label = "placeholder", placeholder = "Enter the page number...")
 
@@ -20,12 +20,12 @@ class ModalPage(discord.ui.Modal, title = "Jump to page"):
       return
     if interaction.message.flags.ephemeral:
       await interaction.response.defer()
-      await interaction.edit_original_response(embed = self.embeds[int(response)], view = PaginateButtons(embeds = self.embeds, page = int(response), author_ids = self.author_ids, timeout = self.timeout, interaction = interaction))
+      await interaction.edit_original_response(embed = self.embeds[int(response)], view = paginateButtons(embeds = self.embeds, page = int(response), author_ids = self.author_ids, timeout = self.timeout, interaction = interaction))
     else:
-      await interaction.message.edit(embed = self.embeds[int(response)], view = PaginateButtons(embeds = self.embeds, page = int(response), author_ids = self.author_ids, timeout = self.timeout, interaction = interaction))
+      await interaction.message.edit(embed = self.embeds[int(response)], view = paginateButtons(embeds = self.embeds, page = int(response), author_ids = self.author_ids, timeout = self.timeout, interaction = interaction))
       await interaction.response.defer()
 
-class PaginateButtons(discord.ui.View):
+class paginateButtons(discord.ui.View):
   def __init__(self, embeds: list[discord.Embed], page: int, author_ids: list[int], timeout: int, interaction: discord.Interaction = None, timedout: bool = False):
     super().__init__(timeout = timeout)
     self.embeds = embeds
@@ -64,9 +64,9 @@ class PaginateButtons(discord.ui.View):
       return
     if interaction.message.flags.ephemeral:
       await interaction.response.defer()
-      await interaction.edit_original_response(embed = self.embeds[0], view = PaginateButtons(embeds = self.embeds, author_ids = self.author_ids, page = 0, timeout = self.timeout, interaction = interaction))    
+      await interaction.edit_original_response(embed = self.embeds[0], view = paginateButtons(embeds = self.embeds, author_ids = self.author_ids, page = 0, timeout = self.timeout, interaction = interaction))    
     else:
-      await interaction.message.edit(embed = self.embeds[0], view = PaginateButtons(embeds = self.embeds, author_ids = self.author_ids, page = 0, timeout = self.timeout, interaction = interaction))
+      await interaction.message.edit(embed = self.embeds[0], view = paginateButtons(embeds = self.embeds, author_ids = self.author_ids, page = 0, timeout = self.timeout, interaction = interaction))
       await interaction.response.defer()
 
   @discord.ui.button(emoji = "⬅️", style=discord.ButtonStyle.grey, disabled = True)
@@ -75,16 +75,16 @@ class PaginateButtons(discord.ui.View):
       return
     if interaction.message.flags.ephemeral:
       await interaction.response.defer()
-      await interaction.edit_original_response(embed = self.embeds[self.page-1], view = PaginateButtons(embeds = self.embeds, page = self.page-1, author_ids = self.author_ids, timeout = self.timeout, interaction = interaction))      
+      await interaction.edit_original_response(embed = self.embeds[self.page-1], view = paginateButtons(embeds = self.embeds, page = self.page-1, author_ids = self.author_ids, timeout = self.timeout, interaction = interaction))      
     else:
-      await interaction.message.edit(embed = self.embeds[self.page-1], view = PaginateButtons(embeds = self.embeds, page = self.page-1, author_ids = self.author_ids, timeout = self.timeout, interaction = interaction))
+      await interaction.message.edit(embed = self.embeds[self.page-1], view = paginateButtons(embeds = self.embeds, page = self.page-1, author_ids = self.author_ids, timeout = self.timeout, interaction = interaction))
       await interaction.response.defer() 
 
   @discord.ui.button(label = "1", style = discord.ButtonStyle.blurple)
   async def buttonModal(self, interaction: discord.Interaction, button: discord.ui.Button):
     if self.author_ids and interaction.user.id not in self.author_ids:
       return
-    await interaction.response.send_modal(ModalPage(embeds = self.embeds, author_ids = self.author_ids, timeout = self.timeout))
+    await interaction.response.send_modal(modal(embeds = self.embeds, author_ids = self.author_ids, timeout = self.timeout))
 
   @discord.ui.button(emoji = "➡️", style=discord.ButtonStyle.grey, disabled = True)
   async def buttonPaginateRight(self, interaction: discord.Interaction, button: discord.ui.Button):
@@ -92,9 +92,9 @@ class PaginateButtons(discord.ui.View):
       return
     if interaction.message.flags.ephemeral:
       await interaction.response.defer()
-      await interaction.edit_original_response(embed = self.embeds[self.page+1], view = PaginateButtons(embeds = self.embeds, page = self.page+1, author_ids = self.author_ids, timeout = self.timeout, interaction = interaction))      
+      await interaction.edit_original_response(embed = self.embeds[self.page+1], view = paginateButtons(embeds = self.embeds, page = self.page+1, author_ids = self.author_ids, timeout = self.timeout, interaction = interaction))      
     else:
-      await interaction.message.edit(embed = self.embeds[self.page+1], view = PaginateButtons(embeds = self.embeds, page = self.page+1, author_ids = self.author_ids, timeout = self.timeout, interaction = interaction))      
+      await interaction.message.edit(embed = self.embeds[self.page+1], view = paginateButtons(embeds = self.embeds, page = self.page+1, author_ids = self.author_ids, timeout = self.timeout, interaction = interaction))      
       await interaction.response.defer() 
 
   @discord.ui.button(emoji = "⏩", style=discord.ButtonStyle.grey, disabled = True)
@@ -103,18 +103,32 @@ class PaginateButtons(discord.ui.View):
       return
     if interaction.message.flags.ephemeral:
       await interaction.response.defer()
-      await interaction.edit_original_response(embed = self.embeds[len(self.embeds)-1], view = PaginateButtons(embeds = self.embeds, author_ids = self.author_ids, page = len(self.embeds)-1, timeout = self.timeout, interaction = interaction))
+      await interaction.edit_original_response(embed = self.embeds[len(self.embeds)-1], view = paginateButtons(embeds = self.embeds, author_ids = self.author_ids, page = len(self.embeds)-1, timeout = self.timeout, interaction = interaction))
     else:
-      await interaction.message.edit(embed = self.embeds[len(self.embeds)-1], view = PaginateButtons(embeds = self.embeds, author_ids = self.author_ids, page = len(self.embeds)-1, timeout = self.timeout, interaction = interaction))
+      await interaction.message.edit(embed = self.embeds[len(self.embeds)-1], view = paginateButtons(embeds = self.embeds, author_ids = self.author_ids, page = len(self.embeds)-1, timeout = self.timeout, interaction = interaction))
       await interaction.response.defer()
 
-class Paginator():
+class paginator():
   def __init__(self, embeds: list[discord.Embed], author_ids: list[int], timeout: int, interaction: discord.Interaction = None):
     self.embeds = embeds
     self.embed = self.embeds[0]
     self.author_ids = author_ids
     self.timeout = timeout
-    self.view = PaginateButtons(embeds = self.embeds, author_ids = self.author_ids, page = 0, timeout = self.timeout)
+    self.view = paginateButtons(embeds = self.embeds, author_ids = self.author_ids, page = 0, timeout = self.timeout)
 
-async def Paginate(embeds: list[discord.Embed], author_ids: list[int] = None, timeout: int = 90) -> Paginator:
-  return Paginator(embeds, author_ids, timeout)
+async def paginate(embeds: list[discord.Embed], author_ids: list[int] = None, timeout: int = 90) -> paginator:
+  """|coro| 
+  
+  Returns a dpy_components.paginator object that contains the first embed
+  and the view associated with it.
+  
+  Parameters 
+  ----------- 
+  embeds: List[:class:`discord.Embed`]
+      A list of embeds to paginate.
+  author_ids: List[:class:`int`] 
+      An optional list of author IDs who can interact with the view.
+  timeout: :class:`int`
+      An optional integer for specifying the timeout (in seconds). Defaults to 90.
+  """
+  return paginator(embeds, author_ids, timeout)
